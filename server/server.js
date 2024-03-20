81977bbf56b3f49b6b39d59a2152400929224abd
victor.dabija <victor.dabija@bpay.md>
2024-03-21 01:01:48 +0200

    feat: added client port number

--- server/server.js
+++ server/server.js
@@ -3,8 +3,10 @@ const net = require('net');
 const port = '8080'
 
 const server = net.createServer(socket => {
-    const clientAddress = socket.remoteAddress
-    console.log(`Client connected: ${clientAddress}`);
+    const clientAddress = socket.remoteAddress;
+    const clientPort = socket.remotePort;
+
+    console.log(`Client connected: ${clientAddress}:${clientPort}`);
 
     socket.on('data', data => {
         console.log('Received:', data.toString());
